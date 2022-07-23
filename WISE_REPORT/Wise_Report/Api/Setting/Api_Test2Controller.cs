5077595c2811e08e72046ef19e2aa37c9c87597d
phamquanghuy.94hubt@gmail.com <>
2022-07-23 11:51:54 +0700

    thanhlt_23/07/2022_lần 3
    test2

---
+++ WISE_REPORT/Wise_Report/Api/Setting/Api_Test2Controller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Wise_Report.Api.Setting
+{
+    public class Api_Test2Controller : ApiController
+    {
+        #region hello
+        #endregion
+    }
+}
